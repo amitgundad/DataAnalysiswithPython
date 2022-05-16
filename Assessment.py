d0af0296ca664582a7ff7202110b48d308c420dc
Amitkumar V. Gundad <7025101+amitgundad@users.noreply.github.com>
2022-05-16 11:49:37 +0530

    Delete Assessment.py

--- Assessment.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler,PolynomialFeatures
-%matplotlib inline
