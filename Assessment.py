55e78053fe32bba697bc554b42026e101784e4a0
Amitkumar V. Gundad <7025101+amitgundad@users.noreply.github.com>
2022-05-16 11:47:42 +0530

    Create Assessment.py

---
+++ Assessment.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler,PolynomialFeatures
+%matplotlib inline
